07afd45cd71242ed8aecc8fbbf2adf964560b12b
Can <ozdenkadir81@gmail.com>
2022-05-13 00:10:29 +0300

    pygame-art-of-math

---
+++ EquationsArt/equatations/Equations.py
@@ -0,0 +1,4 @@
+EQUATIONS = [
+    ['math.sin(alpha+i/counter)*math.cos(alpha+i-counter2)*300',
+    'math.sin(alpha-i/counter)*math.sin(alpha+i-counter2)*300']
+]
---
+++ README.md
@@ -0,0 +1,1 @@
+# pygame-art-of-math
